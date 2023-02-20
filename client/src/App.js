08d30db59f2d48013b1dc7043b39a3dfe5c505c6
Ahae KIM <kimaae@gmail.com>
2023-02-20 22:58:44 +0900

    feat: api 테스트 post, delete 요청 메소드 작성

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import API_TEST from './API_TEST.jsx';
 import './App.css';
 
-const BASE_URL = 'https://c691-211-184-28-120.jp.ngrok.io/hello';
-
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const res = await axios.get(BASE_URL);
-        console.log(res.data);
-        setData(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getData();
-  }, []);
-  console.log(data);
-
-  return (
-    <>
-      <p>여기 아래에 서버에서 보내준 데이터가 뜬다면 성공입니다 :)</p>
-      <p>{data}</p>
-    </>
-  );
+  return <API_TEST />;
 }
 
 export default App;
