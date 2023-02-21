2763c0132bfd1c172a306d7e922055d9f5120c41
Ahae KIM <kimaae@gmail.com>
2023-02-21 13:43:36 +0900

    design: 기본 레이아웃 컴포넌트 생성

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import styles from './Nav.module.css';
+
+function Nav() {
+  return <nav id={styles.nav}>Navigation menu</nav>;
+}
+
+export default Nav;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import styles from './Home.module.css';
+
+function Home() {
+  return <main id={styles.home}>Home Page</main>;
+}
+
+export default Home;
