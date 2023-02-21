5a094ea7f97036da8d18e3c64c77012e2555408f
Ahae KIM <kimaae@gmail.com>
2023-02-21 13:27:18 +0900

    design: css 초기화

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 // import { useState } from 'react';
 // import API_TEST from './API_TEST.jsx';
+import GlobalStyles from './GlobalStyles';
 import Header from './components/Header.jsx';
+
 import './App.css';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
 
   return (
     <>
+      <GlobalStyles />
       <Header />
     </>
   );
---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,9 @@
+// css 초기화 설정
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset} 
+`;
+
+export default GlobalStyles;
