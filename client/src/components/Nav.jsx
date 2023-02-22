e8844420576d9b126c6a5153f3a908cce522083f
Ahae KIM <kimaae@gmail.com>
2023-02-22 12:13:52 +0900

    fix: package-lock.json 파일 revert 버전 적용 및 nav 파일 병합

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,31 +1,26 @@
 import styles from './Nav.module.css';
 import navExploreLogo from '../assets/navExplore.png';
 import navILogo from '../assets/navCollectives.png';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; //폰트어썸 임포트
-// import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons'; //폰트어썸 내의 아메리카 이모지 임포트
 
 function Nav() {
   return (
     <nav id={styles.nav}>
       <div>
-        <ol id={styles.ol}>
-          <li id={styles.li1}>Home</li>
-          <li id={styles.li1}>PUBLIC</li>
+        <ol className={styles.ol}>
+          <li className={styles.li1}>Home</li>
+          <li className={styles.li1}>PUBLIC</li>
           <li>
             <ol>
-              <li id={styles.li2}>
-                {/* <FontAwesomeIcon icon={faEarthAmericas} /> */}
-                Questions
-              </li>
-              <li id={styles.li3}>Tags</li>
-              <li id={styles.li3}>Users</li>
-              <li id={styles.li3}>Companies</li>
-              <li id={styles.li4}>
+              <li className={styles.li2}>Questions</li>
+              <li className={styles.li2}>Tags</li>
+              <li className={styles.li2}>Users</li>
+              <li className={styles.li2}>Companies</li>
+              <li className={styles.li1}>
                 COLLECTIVES
-                <img id={styles.img} src={navILogo} alt='nnavCollectievs' />
+                <img className={styles.imgI} src={navILogo} alt='navCollectives' />
               </li>
-              <li id={styles.li5}>
-                <img id={styles.img} src={navExploreLogo} alt='navExploreLogo' />
+              <li className={styles.li1}>
+                <img className={styles.img} src={navExploreLogo} alt='navExploreLogo' />
                 <span>Explore Collectives</span>
               </li>
             </ol>
