fb69a78a5f168a5101702e657ce868079e4debde
Ahae KIM <kimaae@gmail.com>
2023-02-22 23:00:00 +0900

    feat: header 기본 레이아웃 구현

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,43 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from './UI/Button.jsx';
 import styles from './Header.module.css';
 
 function Header() {
-  const navigate = useNavigate();
-
-  const goTo = (path) => {
-    navigate(path);
-  };
-
   return (
     <header id={styles.header}>
-      <button onClick={() => goTo('/')}>스택오버플로우 로고</button>
-      <button onClick={() => goTo('/questions')}>Questions</button>
-      <Button
-        text='Log in'
-        path='/users/login'
-        addStyle={{
-          borderColor: 'var(--button-bd-color)',
-          backgroundColor: 'var(--button-secondary-bg-color)',
-          color: 'var(--button-filled-color)'
-        }}
-      />
-      <Button text='Sign up' path='/users/signup' />
+      <div className={styles.container}>
+        <Link to='/' className={styles.logo}>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/0/02/Stack_Overflow_logo.svg'
+            alt='logo'
+            className={styles.logoImg}
+          ></img>
+        </Link>
+        <ol className={styles.navigation}>
+          <li>
+            <Link to='/questions'>Products</Link>
+          </li>
+        </ol>
+        <form className={styles.searchBar}>
+          <input placeholder='Search...'></input>
+        </form>
+        <ol className={styles.menubar}>
+          <li>
+            <Button
+              text='Log in'
+              path='/users/login'
+              addStyle={{
+                borderColor: 'var(--button-bd-color)',
+                backgroundColor: 'var(--button-secondary-bg-color)',
+                color: 'var(--button-filled-color)'
+              }}
+            />
+          </li>
+          <li>
+            <Button text='Sign up' path='/users/signup' />
+          </li>
+        </ol>
+      </div>
     </header>
   );
 }
