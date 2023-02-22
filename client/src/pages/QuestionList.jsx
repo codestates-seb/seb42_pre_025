4955f105ee2bc7e61feda99638fabffe002ce9b5
benidene <dlgusehd09@naver.com>
2023-02-22 16:22:28 +0900

    feat: 질문목록페이지1차구현

--- client/src/pages/QuestionList.jsx
+++ client/src/pages/QuestionList.jsx
@@ -7,11 +7,31 @@ function QuestionList() {
     <>
       <div className={styles.container}>
         <Nav />
-        <ul id={styles.list}>
-          <li>아이템 1</li>
-          <li>아이템 2</li>
-          <li>아이템 3</li>
-        </ul>
+        <div className={styles.div}>
+          <div className={styles.border}>
+            <div className={styles.allQuestionsBox}>
+              <div className={styles.allQuestions}>All Questions</div>
+              <button className={styles.askQuesion}>Ask Question</button>
+            </div>
+            <div className={styles.div2}>23,510,472 questions</div>
+          </div>
+          <div className={styles.questionList}>
+            질문목록
+            <div className={styles.question}>
+              <div></div>
+              <div className={styles.content}></div>
+            </div>
+          </div>
+          <div className={styles.page}>
+            <div className={styles.pageButton}>1</div>
+            <div className={styles.pageButton}>2</div>
+            <div className={styles.pageButton}>3</div>
+            <div className={styles.pageButton}>4</div>
+            <div className={styles.pageButton}>5</div>
+            <div className={styles.pagedot}>....</div>
+            <div className={styles.pageNext}>Next</div>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
