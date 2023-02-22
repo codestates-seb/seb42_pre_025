b1d4243a107c98c7100ba93dd8864b56cb71b4b2
jia222 <wltkd0515@naver.com>
2023-02-22 18:15:56 +0900

    feat: 회원가입 페이지 작업 중

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,82 @@
+import { useState } from 'react';
 import styles from './Signup.module.css';
+// import icon from ''
+import AboutSignup from './AboutSignup.jsx';
 
 function Signup() {
-  return <main id={styles.main}>회원가입 페이지</main>;
+  const [isSignup, setIsSignup] = useState(false);
+
+  function handleSwitch() {
+    setIsSignup(!isSignup);
+  }
+
+  return (
+    <section className={styles.signupContainer}>
+      {/* <'signup-section'> */}
+      {isSignup && <AboutSignup />}
+      <div className={styles.signupContainer}>
+        {/* 'signup-section'> */}
+        {/* { !isSignup && <img src = {icon} alt = 'stack overflow' className='login-logo'/>} */}
+        <from>
+          {isSignup && (
+            <label htmlFor='name'>
+              <h4>Display Name</h4>
+              <input type='text' id='name' name='name' />
+            </label>
+          )}
+          <label htmlFor='email'>
+            <h4>Email</h4>
+            <input type='email' name='email' id='email' />
+          </label>
+          <label htmlFor='password'>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <h4>Password</h4>
+              {!isSignup && <p style={{ color: '#007ac6', fontSize: '13px' }}>forgot password</p>}
+            </div>
+            <input type='password' name='password' id='password' />
+            {isSignup && (
+              <p style={{ color: '#666767', fontSize: '13px' }}>
+                Passwords must contain at least eight
+                <br />
+                characters, including at least 1<br /> letter and 1 number.
+              </p>
+            )}
+          </label>
+          {isSignup && (
+            <label htmlFor='check'>
+              <input type='checkbox' id='check' />
+              <p style={{ fontSize: '13px' }}>
+                Opt-in to receive occasional product
+                <br /> updates, user research invitations,
+                <br /> company announcements, and digests.
+              </p>
+            </label>
+          )}
+          <button type='submit' className='auth-btn'>
+            {isSignup ? 'Sign up' : 'Log in'}
+          </button>
+          {isSignup && (
+            <p style={{ color: '#666767', fontSize: '13px' }}>
+              By clicking “Sign up”, you agree to our
+              <span style={{ color: '#007ac6' }}>
+                {' '}
+                terms of
+                <br /> service
+              </span>
+              ,<span style={{ color: '#007ac6' }}> privacy policy</span> and
+              <span style={{ color: '#007ac6' }}> cookie policy</span>
+            </p>
+          )}
+        </from>
+        <p>
+          {isSignup ? 'Already have an account?' : "Don't have an account?"}
+          <button type='button' className='handle-switch-btn' onClick={handleSwitch}>
+            {isSignup ? 'Log in' : 'sign up'}
+          </button>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Signup;
