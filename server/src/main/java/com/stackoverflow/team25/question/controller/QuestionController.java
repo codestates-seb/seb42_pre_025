0bf81b396173b1ba74acfcb0afd8018e2d5c054e
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2023-02-24 09:53:53 +0900

    feat: question mapping
    question >- user mapped

--- server/src/main/java/com/stackoverflow/team25/question/controller/QuestionController.java
+++ server/src/main/java/com/stackoverflow/team25/question/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.stackoverflow.team25.question.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.stackoverflow.team25.answer.dto.AnswerDto;
 import com.stackoverflow.team25.answer.entity.Answer;
 import com.stackoverflow.team25.answer.mapper.AnswerMapper;
@@ -8,10 +9,16 @@ import com.stackoverflow.team25.question.dto.QuestionDto;
 import com.stackoverflow.team25.question.entity.Question;
 import com.stackoverflow.team25.question.mapper.QuestionMapper;
 import com.stackoverflow.team25.question.service.QuestionService;
+import com.stackoverflow.team25.user.dto.UserDto;
+import com.stackoverflow.team25.user.entity.User;
+import com.stackoverflow.team25.user.mapper.UserMapper;
+import com.stackoverflow.team25.user.service.UserService;
 import com.stackoverflow.team25.utils.UriCreator;
 import com.stackoverflow.team25.dto.MultiResponseDto;
 import com.stackoverflow.team25.dto.SingleResponseDto;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -34,10 +41,12 @@ public class QuestionController {
     private final QuestionMapper mapper;
     private final AnswerService answerService;
     private final AnswerMapper answerMapper;
+    private final UserMapper userMapper;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity postQuestion(@Valid @RequestBody QuestionDto.QuestionPostDto questionPostDto) {
-        Question question = questionService.createQuestion(mapper.questionPostDtoToQuestion(questionPostDto));
+        Question question = questionService.createQuestion(mapper.questionPostDtoToQuestion(questionPostDto),questionPostDto.getUserId());
         URI location = UriCreator.createUri(QUESTION_DEFAULT_URL, question.getQuestionId());
 
         return ResponseEntity.created(location).build();
@@ -49,14 +58,16 @@ public class QuestionController {
         questionPatchDto.setQuestionId(questionId);
         Question question = questionService.updateQuestion(mapper.questionPatchDtoToQuestion(questionPatchDto));
         QuestionDto.QuestionResponseDto response = mapper.questionToQuestionResponseDto(question);
+        response.setUserDto(UserDto.Response.builder().user(question.getUser()).build());
 
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
-
     @GetMapping("/{question-id}")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public ResponseEntity getQuestion(@PathVariable("question-id") long questionId) {
         Question question = questionService.findQuestion(questionId);
         QuestionDto.QuestionResponseDto response = mapper.questionToQuestionResponseDto(question);
+        response.setUserDto(UserDto.Response.builder().user(question.getUser()).build());
 
         return new ResponseEntity<>(new SingleResponseDto<>(response),HttpStatus.OK);
     }
@@ -66,6 +77,10 @@ public class QuestionController {
         Page<Question> pageQuestions = questionService.findQuestions(pageable);
         List<Question> questions = pageQuestions.getContent();
         List<QuestionDto.QuestionResponseDto> responses = mapper.questionsToQuestionResponseDtos(questions);
+        for (QuestionDto.QuestionResponseDto response : responses) {
+            Question question = questionService.findQuestion(response.getQuestionId());
+            response.setUserDto(UserDto.Response.builder().user(question.getUser()).build());
+        }
 
         return new ResponseEntity<>(new MultiResponseDto<>(responses,pageQuestions),HttpStatus.OK);
     }
