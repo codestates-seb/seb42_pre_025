9456b0528327360d1d175472de9e0fd061b3f56e
Ahae KIM <kimaae@gmail.com>
2023-02-24 10:19:45 +0900

    fix: 환경변수 url 수정

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import styles from './Login.module.css';
 function Login() {
   const [data, setData] = useState([]);
   const { REACT_APP_BASE_URL: BASE_URL } = process.env;
-  const URL = `${BASE_URL}/users?page=1&size=5&sort=userId,desc`;
+  const URL = `${BASE_URL}/api/users?page=1&size=5&sort=userId,desc`;
 
   console.log(URL);
 
