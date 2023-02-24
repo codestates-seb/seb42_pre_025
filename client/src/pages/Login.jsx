c35871c3dfbc30da7da2b61486c82358864b08c3
Ahae KIM <kimaae@gmail.com>
2023-02-24 09:59:35 +0900

    fix: 로그인 페이지 get 요청 쿠키 테스트 수정

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,9 @@ import styles from './Login.module.css';
 function Login() {
   const [data, setData] = useState([]);
   const { REACT_APP_BASE_URL: BASE_URL } = process.env;
-  const URL = `${BASE_URL}/users`;
+  const URL = `${BASE_URL}/users?page=1&size=5&sort=userId,desc`;
+
+  console.log(URL);
 
   useEffect(() => {
     getData(URL, setData);
