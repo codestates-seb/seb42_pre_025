e1c2bf865560008caaecb854a7e4332311d819f5
Ahae KIM <kimaae@gmail.com>
2023-02-27 16:01:15 +0900

    feat: 로그아웃 post 요청 구현

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import GlobalStyles from './GlobalStyles';
 import './App.css';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   console.log(setIsLoggedIn);
 
   return (
