25d0f52f0031eee4ee2d63bc1a0c1e34cd1b5423
benidene <dlgusehd09@naver.com>
2023-02-27 14:12:29 +0900

    fix: 자식 셀렉터 사용 금지금지

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,48 +15,48 @@ function Footer() {
           <ol className={styles.title}>
             STACK OVERFLOW
             <li className={styles.secondTitle}>Questions</li>
-            <li>Help</li>
+            <li className={styles.third}>Help</li>
           </ol>
         </div>
         <div className={styles.thirdBox}>
           <ol className={styles.title}>
             PRODUCTS
             <li className={styles.secondTitle}>Teams</li>
-            <li>Advertising</li>
-            <li>Collectives</li>
-            <li>Talent</li>
+            <li className={styles.third}>Advertising</li>
+            <li className={styles.third}>Collectives</li>
+            <li className={styles.third}>Talent</li>
           </ol>
         </div>
         <div className={styles.forthBox}>
           <ol className={styles.title}>
             COMPANY
             <li className={styles.secondTitle}>About</li>
-            <li>Press</li>
-            <li>Work Here</li>
-            <li>Legal</li>
-            <li>Privacy Policy</li>
-            <li>Terms of Service</li>
-            <li>Contact Us</li>
-            <li>Cookie Setting</li>
-            <li>Cookie Policy</li>
+            <li className={styles.third}>Press</li>
+            <li className={styles.third}>Work Here</li>
+            <li className={styles.third}>Legal</li>
+            <li className={styles.third}>Privacy Policy</li>
+            <li className={styles.third}>Terms of Service</li>
+            <li className={styles.third}>Contact Us</li>
+            <li className={styles.third}>Cookie Setting</li>
+            <li className={styles.third}>Cookie Policy</li>
           </ol>
         </div>
         <div className={styles.fifthBox}>
           <ol className={styles.title}>
             STACK EXCHANGE NETWORK
             <li className={styles.secondTitle}>Technology</li>
-            <li>Culture & recreation</li>
-            <li>Life & arts</li>
-            <li>Science</li>
-            <li>Professional</li>
-            <li>Business</li>
+            <li className={styles.third}>Culture & recreation</li>
+            <li className={styles.third}>Life & arts</li>
+            <li className={styles.third}>Science</li>
+            <li className={styles.third}>Professional</li>
+            <li className={styles.third}>Business</li>
             <div className={styles.api}>API</div>
-            <li>Data</li>
+            <li className={styles.third}>Data</li>
           </ol>
         </div>
         <div className={styles.sixthBox}>
           <ul className={styles.sixthBoxSNS}>
-            <li>Blog</li>
+            <li className={styles.snsFirst}>Blog</li>
             <li className={styles.snsMargin}>Facebook</li>
             <li className={styles.snsMargin}>Twitter</li>
             <li className={styles.snsMargin}>Linkedin</li>
