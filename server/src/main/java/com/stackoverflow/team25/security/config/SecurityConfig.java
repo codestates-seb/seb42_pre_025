7ff91e6ab5b2eda05a6d16a3f97b73c300861a0b
qushe8r <qushe8r@gmail.com>
2023-02-28 12:42:47 +0900

    feat: 로그인 설정

--- server/src/main/java/com/stackoverflow/team25/security/config/SecurityConfig.java
+++ server/src/main/java/com/stackoverflow/team25/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.stackoverflow.team25.security.config;
 import com.stackoverflow.team25.security.filter.JwtAuthenticationFilter;
 import com.stackoverflow.team25.security.filter.JwtVerificationFilter;
 import com.stackoverflow.team25.security.handler.JwtAuthenticationSuccessHandler;
+import com.stackoverflow.team25.security.handler.OAuth2SuccessHandler;
 import com.stackoverflow.team25.security.jwt.JwtTokenizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -16,6 +17,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -50,6 +52,11 @@ public class SecurityConfig {
                 .headers().frameOptions().sameOrigin();
         http
                 .apply(new CustomFilterConfigurer());
+        http
+                .oauth2Login().successHandler(new OAuth2SuccessHandler(jwtTokenizer));
+        http
+                .logout()
+                .logoutUrl("/api/logout");
 
         return http.build();
     }
@@ -81,14 +88,12 @@ public class SecurityConfig {
             AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
 
             JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
-            jwtAuthenticationFilter.setFilterProcessesUrl("/api/auth/login");
+            jwtAuthenticationFilter.setFilterProcessesUrl("/api/login");
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(new JwtAuthenticationSuccessHandler());
 
             JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer);
-
-
             http
-                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAt(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                     .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
         }
     }
