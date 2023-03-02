d22f2d45fbd0b75e2762f7085de10d1b3dd35860
qushe8r <qushe8r@gmail.com>
2023-03-02 21:42:42 +0900

    hotfix: 로그아웃 기능 수정
    200으로 내려주면 프론트에서 루트페이지로 이동

--- server/src/main/java/com/stackoverflow/team25/security/config/SecurityConfig.java
+++ server/src/main/java/com/stackoverflow/team25/security/config/SecurityConfig.java
@@ -2,37 +2,26 @@ package com.stackoverflow.team25.security.config;
 
 import com.stackoverflow.team25.security.filter.JwtAuthenticationFilter;
 import com.stackoverflow.team25.security.filter.JwtVerificationFilter;
+import com.stackoverflow.team25.security.handler.CustomLogoutHandler;
 import com.stackoverflow.team25.security.handler.JwtAuthenticationSuccessHandler;
 import com.stackoverflow.team25.security.handler.OAuth2SuccessHandler;
 import com.stackoverflow.team25.security.jwt.JwtTokenizer;
 import com.stackoverflow.team25.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 
 @Configuration
@@ -73,11 +62,10 @@ public class SecurityConfig {
                 .apply(new CustomFilterConfigurer());
         http
                 .oauth2Login().successHandler(new OAuth2SuccessHandler(userService));
-        //TODO:: 로그아웃 핸들러 >> redirect "/" >> localhost:3000
         http
                 .logout()
                 .logoutUrl("/api/logout")
-                .logoutSuccessUrl("/");
+                .logoutSuccessHandler(new CustomLogoutHandler());
 
         return http.build();
     }
